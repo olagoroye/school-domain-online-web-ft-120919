3df55bf1fcffa5a411c404ca07a060c963a9bfcb
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-22 23:41:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,36 +11,36 @@
 # end
 # end
 
-class School
+# class School
 
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
-  end
+#   def initialize(school_name)
+#     @school_name = school_name
+#     @roster = {}
+#   end
 
-  def roster
-    @roster
-  end
+#   def roster
+#     @roster
+#   end
 
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-    end
-    @roster[grade] << name
-  end
+#   def add_student(name, grade)
+#     @student_name = name
+#     @grade = grade
+#     if @roster.include?(grade) == false
+#       @roster[grade] = []
+#     end
+#     @roster[grade] << name
+#   end
 
-  def grade(number)
-    @roster[number]
-  end
+#   def grade(number)
+#     @roster[number]
+#   end
 
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-  end
-end
+#   def sort
+#     @roster.each do |grade, name|
+#       @roster[grade] = name.sort
+#     end
+#   end
+# end
 
 
 
